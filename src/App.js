647800a4d6a7e4fbbc9ec810fd6be61cb48256df
Hamza TOUHARDJI <hamza@motema.fr>
2021-10-31 17:29:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import MyMap from './components/my-map';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [submit, setSubmit] = useState("false");
+  const [posts, setPosts] = useState([]);
+  const [ip, setIP] = useState('');
+  const [lat, setLat] = useState('');
+  const [lng, setLng] = useState('');
+  const [city, setCity] = useState('');
+
+
+
+  const fetchPost = async () => {
+    console.log("Votre ip : " + ip);
+    /**
+     * var url = "https://geo.ipify.org/api/v2/country,city?apiKey=at_g2FhghrbuPCUqHGa2OZt1z0MyCtVD&ipAddress=" + ip
+    const response = await fetch(url);
+    const data = await response.json();
+    setPosts(data.location);
+     */
+  };
+
+  useEffect(() => {
+    getData();
+  });
+
+  const getData = async () => {
+    const response = await fetch(
+      "https://geolocation-db.com/json/"
+    );
+    const data = await response.json();
+    setIP(data.IPv4);
+    setCity(data.city)
+  };
+
+  const handleChange = e => {
+    if (submit === true) {
+      setIP(e.target.value);
+    }
+
+  };
+
+  const handleSubmit = () => {
+    setSubmit(true);
+    fetchPost();
+  };
+
   return (
-    <MyMap />
+    <div>
+      <div className="header">
+        <h1 className="title">IP Adresse Tracker</h1>
+        <form action="/action_page.php" className="form">
+          <input type="text" placeholder="Email" onChange={handleChange} />
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+
+
+      </div>
+      <div className="information">
+        <div>
+          <p>ip address</p> <p>{value === '' ? ip : value}</p>
+        </div>
+        <div>
+          <p>location</p> <p>{city}</p>
+        </div>
+        <div>
+          <p>timezone</p> <p>nan</p>
+        </div>
+        <div>
+          <p>isp</p> <p>nan</p>
+        </div>
+      </div>
+
+
+      <div className="map">
+        {
+          /*
+          <MyMap lng={4.835659} lat={45.764043} />
+         */
+        }
+      </div>
+    </div>
   );
 }
 
--- src/components/my-map.jsx
+++ src/components/my-map.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react'; 
+import React, { useEffect } from 'react';
 import './my-map.scss';
+import L from 'leaflet';
 
-function MyMap() {
+function MyMap(props) {
+  let mapContainer;
 
   useEffect(() => {
-    console.log('This is called when the component is mounted!');
-  }, []); // passing an empty array as second argument triggers the callback in useEffect only after the initial render thus replicating `componentDidMount` lifecycle behaviour
+    const initialState = {
+      lng: props.lng,
+      lat: props.lat,
+      zoom: 10
+    };
+
+    const map = L.map(mapContainer).setView([initialState.lat, initialState.lng], initialState.zoom);
+
+    var myAPIKey = '43e8953f5a0640dd8fc416b83ed3d7c9';
+
+    var isRetina = L.Browser.retina;
+    var baseUrl = "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}";
+    var retinaUrl = "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey={apiKey}";
+
+    L.tileLayer(isRetina ? retinaUrl : baseUrl, {
+      attribution: 'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>',
+      apiKey: myAPIKey,
+      maxZoom: 20,
+      id: 'osm-bright',
+    }).addTo(map);
+
+  }, [mapContainer]);
 
   return (
-    <div className="map-container">
-      <div className="placeholder">
-        <span className="placeholder-text">The map will be displayed here</span>
-      </div>
+    <div className="map-container" ref={el => mapContainer = el}>
     </div>
   )
 }
